614e4fa25f295a2c97d460252bbd5b01ccf4f4d3
Jared <Jared@LAPTOP-GMS70DG6>
2025-06-28 20:58:53 +0800

    Add project files.

---
+++ ADETAct1/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+class Program
+{
+    
+    static int AddNumbers(int a, int b)
+    {
+        return a + b;
+    }
+
+  
+    static void Main(string[] args)
+    {
+        // Call and display AddNumbers function
+        int sum = AddNumbers(5, 7);
+        Console.WriteLine("Sum: " + sum);
+
+       
+    }
+}
