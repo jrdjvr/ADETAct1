abb42a433f3195d9a984ca2dcb4e98199de7a490
Jared <Jared@LAPTOP-GMS70DG6>
2025-06-28 21:09:53 +0800

    updated the numbers

--- ADETAct1/Program.cs
+++ ADETAct1/Program.cs
@@ -13,7 +13,7 @@ class Program
     static void Main(string[] args)
     {
         // Call and display AddNumbers function
-        int sum = AddNumbers(5, 7);
+        int sum = AddNumbers(13, 8);
         Console.WriteLine("Sum: " + sum);
 
        
